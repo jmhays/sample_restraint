b75464bdce7daffd07beaa424998d6dac1b7d91c
Jennifer M. Hays <jmh5sf@virginia.edu>
2018-09-18 12:38:20 -0400

    Initial commit of EBMetaD implementation using gmxapi.

--- src/pythonmodule/export_plugin.cpp
+++ src/pythonmodule/export_plugin.cpp
@@ -21,6 +21,7 @@
 
 #include "harmonicpotential.h"
 #include "ensemblepotential.h"
+#include "ebmetad.h"
 #include "make_unique.h"
 
 // Make a convenient alias to save some typing...
@@ -144,6 +145,13 @@ std::shared_ptr<gmxapi::MDModule> PyRestraint<plugin::RestraintModule<plugin::En
 {
     return shared_from_this();
 }
+
+template<>
+std::shared_ptr<gmxapi::MDModule> PyRestraint<plugin::RestraintModule<plugin::EBMetaDRestraint>>::getModule()
+{
+    return shared_from_this();
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////
 // New restraints mimicking EnsembleRestraint should specialize getModule() here as above.
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -385,6 +393,133 @@ class EnsembleRestraintBuilder
         std::string name_;
 };
 
+class EBMetaDRestraintBuilder
+{
+    public:
+        explicit EBMetaDRestraintBuilder(py::object element)
+        {
+            name_ = py::cast<std::string>(element.attr("name"));
+            assert(!name_.empty());
+
+            // It looks like we need some boilerplate exceptions for plugins so we have something to
+            // raise if the element is invalid.
+            assert(py::hasattr(element,
+                               "params"));
+
+            // Params attribute should be a Python list
+            py::dict parameter_dict = element.attr("params");
+            // \todo Check for the presence of these dictionary keys to avoid hard-to-diagnose error.
+
+            // Get positional parameters.
+            py::list sites = parameter_dict["sites"];
+            for (auto&& site : sites)
+            {
+                siteIndices_.emplace_back(py::cast<unsigned long>(site));
+            }
+
+            auto binWidth = py::cast<double>(parameter_dict["binWidth"]);
+            auto minDist = py::cast<double>(parameter_dict["min_dist"]);
+            auto maxDist = pybind11::cast<double>(parameter_dict["max_dist"]);
+
+            std::vector<std::vector<double>> forces{};
+            py::list tmpForceTable = parameter_dict["force_table"];
+            for (auto&& myvec : tmpForceTable){
+                forces.push_back(py::cast<std::vector<double>>(myvec));
+            }
+            auto distanceCounts = pybind11::cast<std::vector<unsigned long>>(parameter_dict["distance_counts"]);
+            auto samplePeriod = pybind11::cast<double>(parameter_dict["sample_period"]);
+            auto k = pybind11::cast<double>(parameter_dict["k"]);
+
+            auto historicalDistancesFilename = pybind11::cast<std::string>(parameter_dict["historical_data_filename"]);
+
+            auto params = plugin::makeEBMetaDParams(forces,
+                    distanceCounts,
+                    binWidth,
+                    samplePeriod,
+                    historicalDistancesFilename,
+                    minDist,
+                    maxDist,
+                    k);
+
+            params_ = std::move(*params);
+
+            // Note that if we want to grab a reference to the Context or its communicator, we can get it
+            // here through element.workspec._context. We need a more general API solution, but this code is
+            // in the Python bindings code, so we know we are in a Python Context.
+            assert(py::hasattr(element,
+                               "workspec"));
+            auto workspec = element.attr("workspec");
+            assert(py::hasattr(workspec,
+                               "_context"));
+            context_ = workspec.attr("_context");
+        }
+
+        /*!
+         * \brief Add node(s) to graph for the work element.
+         *
+         * \param graph networkx.DiGraph object still evolving in gmx.context.
+         *
+         * \todo This may not follow the latest graph building protocol as described.
+         */
+        void build(py::object graph)
+        {
+            // Temporarily subvert things to get quick-and-dirty solution for testing.
+            // Need to capture Python communicator and pybind syntax in closure so EnsembleResources
+            // can just call with matrix arguments.
+
+            // This can be replaced with a subscription and delayed until launch, if necessary.
+            assert(py::hasattr(context_,
+                               "ensemble_update"));
+            // make a local copy of the Python object so we can capture it in the lambda
+            auto update = context_.attr("ensemble_update");
+            // Make a callable with standardizeable signature.
+            const std::string name{name_};
+            auto functor = [update, name](const plugin::Matrix<double>& send,
+                                          plugin::Matrix<double>* receive) {
+                update(send,
+                       receive,
+                       py::str(name));
+            };
+
+            // To use a reduce function on the Python side, we need to provide it with a Python buffer-like object,
+            // so we will create one here. Note: it looks like the SharedData element will be useful after all.
+            auto resources = std::make_shared<plugin::EnsembleResources>(std::move(functor));
+
+            auto potential = PyRestraint<plugin::RestraintModule<plugin::EBMetaDRestraint>>::create(name_,
+                                                                                                     siteIndices_,
+                                                                                                     params_,
+                                                                                                     resources);
+
+            auto subscriber = subscriber_;
+            py::list potentialList = subscriber.attr("potential");
+            potentialList.append(potential);
+
+        };
+
+        /*!
+         * \brief Accept subscription of an MD task.
+         *
+         * \param subscriber Python object with a 'potential' attribute that is a Python list.
+         *
+         * During build, an object is added to the subscriber's self.potential, which is then bound with
+         * system.add_potential(potential) during the subscriber's launch()
+         */
+        void addSubscriber(py::object subscriber)
+        {
+            assert(py::hasattr(subscriber,
+                               "potential"));
+            subscriber_ = subscriber;
+        };
+
+        py::object subscriber_;
+        py::object context_;
+        std::vector<unsigned long int> siteIndices_;
+
+        plugin::ebmetad_input params_;
+
+        std::string name_;
+};
+
 /*!
  * \brief Factory function to create a new builder for use during Session launch.
  *
@@ -407,6 +542,13 @@ std::unique_ptr<EnsembleRestraintBuilder> createEnsembleBuilder(const py::object
 {
     using gmx::compat::make_unique;
     auto builder = make_unique<EnsembleRestraintBuilder>(element);
+    return builder;
+}
+
+std::unique_ptr<EBMetaDRestraintBuilder> createEBMetaDBuilder(const py::object element)
+{
+    using gmx::compat::make_unique;
+    auto builder = make_unique<EBMetaDRestraintBuilder>(element);
     return builder;
 }
 
@@ -579,7 +721,46 @@ PYBIND11_MODULE(myplugin, m) {
     // End EnsembleRestraint
     ///////////////////////////////////////////////////////////////////////////
 
+    //////////////////////////////////////////////////////////////////////////
+    // Begin EBMetaDRestraint
+    //
+    // Define Builder to be returned from ebmetad_restraint Python function defined further down.
+    pybind11::class_<EBMetaDRestraintBuilder> ebmetadBuilder(m,
+                                                               "EBMetaDBuilder");
+    ebmetadBuilder.def("add_subscriber",
+                        &EBMetaDRestraintBuilder::addSubscriber);
+    ebmetadBuilder.def("build",
+                        &EBMetaDRestraintBuilder::build);
 
+    // Get more concise name for the template instantiation...
+    using PyEBMetaD = PyRestraint<plugin::RestraintModule<plugin::EBMetaDRestraint>>;
+
+    // Export a Python class for our parameters struct
+    py::class_<plugin::EBMetaDRestraint::input_param_type> ebmetadParams(m, "EBMetaDRestraintParams");
+    m.def("make_ensemble_params",
+          &plugin::makeEBMetaDParams);
+
+    // API object to build.
+    py::class_<PyEBMetaD, std::shared_ptr<PyEBMetaD >> ebmetad(m, "EBMetaDRestraint");
+    // EnsembleRestraint can only be created via builder for now.
+    ebmetad.def("bind",
+                 &PyEBMetaD::bind,
+                 "Implement binding protocol");
+    /*
+     * To implement gmxapi_workspec_1_0, the module needs a function that a Context can import that
+     * produces a builder that translates workspec elements for session launching. The object returned
+     * by our function needs to have an add_subscriber(other_builder) method and a build(graph) method.
+     * The build() method returns None or a launcher. A launcher has a signature like launch(rank) and
+     * returns None or a runner.
+     */
+
+    // Generate the name operation that will be used to specify elements of Work in gmxapi workflows.
+    // WorkElements will then have namespace: "myplugin" and operation: "ensemble_restraint"
+    m.def("ebmetad_restraint",
+          [](const py::object element) { return createEBMetaDBuilder(element); });
+    //
+    // End EBMetaDRestraint
+    ///////////////////////////////////////////////////////////////////////////
 
 
 }
